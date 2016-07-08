dc7742db6f5e6cc65fe21d4ec932b5162ccadc41
Francesco Guastella <guastella.francesco@gmail.com>
2016-07-08 14:29:09 +0200

    - Updated vk_cpp.h;

--- vulkan/vk_cpp.hpp
+++ vulkan/vk_cpp.hpp
@@ -95,61 +95,58 @@ namespace vk
   {
   public:
     Flags()
-      : m_mask(0)
-    {
-    }
+      : m_mask( 0 )
+    {}
 
-    Flags(BitType bit)
-      : m_mask(static_cast<uint32_t>(bit))
-    {
-    }
+    Flags( BitType bit )
+      : m_mask( static_cast<uint32_t>( bit ) )
+    {}
 
-    Flags(Flags<BitType> const& rhs)
-      : m_mask(rhs.m_mask)
-    {
-    }
+    Flags( Flags<BitType> const& rhs )
+      : m_mask( rhs.m_mask )
+    {}
 
-    Flags<BitType> & operator=(Flags<BitType> const& rhs)
+    Flags<BitType>& operator=( Flags<BitType> const& rhs )
     {
       m_mask = rhs.m_mask;
       return *this;
     }
 
-    Flags<BitType> & operator|=(Flags<BitType> const& rhs)
+    Flags<BitType>& operator|=( Flags<BitType> const& rhs )
     {
       m_mask |= rhs.m_mask;
       return *this;
     }
 
-    Flags<BitType> & operator&=(Flags<BitType> const& rhs)
+    Flags<BitType>& operator&=( Flags<BitType> const& rhs )
     {
       m_mask &= rhs.m_mask;
       return *this;
     }
 
-    Flags<BitType> & operator^=(Flags<BitType> const& rhs)
+    Flags<BitType>& operator^=( Flags<BitType> const& rhs )
     {
       m_mask ^= rhs.m_mask;
       return *this;
     }
 
-    Flags<BitType> operator|(Flags<BitType> const& rhs) const
+    Flags<BitType> operator|( Flags<BitType> const& rhs ) const
     {
       Flags<BitType> result(*this);
       result |= rhs;
       return result;
     }
 
-    Flags<BitType> operator&(Flags<BitType> const& rhs) const
+    Flags<BitType> operator&( Flags<BitType> const& rhs ) const
     {
-      Flags<BitType> result(*this);
+      Flags<BitType> result( *this );
       result &= rhs;
       return result;
     }
 
-    Flags<BitType> operator^(Flags<BitType> const& rhs) const
+    Flags<BitType> operator^( Flags<BitType> const& rhs ) const
     {
-      Flags<BitType> result(*this);
+      Flags<BitType> result( *this );
       result ^= rhs;
       return result;
     }
@@ -159,12 +156,12 @@ namespace vk
       return !m_mask;
     }
 
-    bool operator==(Flags<BitType> const& rhs) const
+    bool operator==( Flags<BitType> const& rhs ) const
     {
       return m_mask == rhs.m_mask;
     }
 
-    bool operator!=(Flags<BitType> const& rhs) const
+    bool operator!=( Flags<BitType> const& rhs ) const
     {
       return m_mask != rhs.m_mask;
     }
@@ -184,19 +181,19 @@ namespace vk
   };
   
   template <typename BitType>
-  Flags<BitType> operator|(BitType bit, Flags<BitType> const& flags)
+  Flags<BitType> operator|( BitType bit, Flags<BitType> const& flags )
   {
     return flags | bit;
   }
   
   template <typename BitType>
-  Flags<BitType> operator&(BitType bit, Flags<BitType> const& flags)
+  Flags<BitType> operator&( BitType bit, Flags<BitType> const& flags )
   {
     return flags & bit;
   }
   
   template <typename BitType>
-  Flags<BitType> operator^(BitType bit, Flags<BitType> const& flags)
+  Flags<BitType> operator^( BitType bit, Flags<BitType> const& flags )
   {
     return flags ^ bit;
   }
