07d73734fb37b52b6a4d82ab69b55bb95379d027
Francesco Guastella <guastella.francesco@gmail.com>
2016-07-25 14:08:24 +0200

    - Minor changes in Findtinyxml2.cmake script;
    - Added .travis.yml

--- cmake/Findtinyxml2.cmake
+++ cmake/Findtinyxml2.cmake
@@ -2,8 +2,23 @@
 # TINYXML2_INCLUDE_DIR
 # TINYXML2_LIBRARIES
 
-find_path( TINYXML2_INCLUDE_DIR NAMES tinyxml2.h )
-find_library( TINYXML2_LIBRARIES NAMES tinyxml2 )
+find_path( TINYXML2_INCLUDE_DIR
+	NAMES tinyxml2.h
+	PATHS
+		/include
+		/usr/include
+		/usr/local/include
+)
+
+find_library( TINYXML2_LIBRARIES
+	NAMES tinyxml tinyxml2
+	PATHS
+		/lib
+		/lib/x86_64-linux-gnu
+		/usr/lib
+		/usr/lib/x86_64-linux-gnu
+		/usr/local/lib
+)
 
 include( FindPackageHandleStandardArgs )
 find_package_handle_standard_args( TINYXML2 DEFAULT_MSG TINYXML2_LIBRARIES TINYXML2_INCLUDE_DIR )
